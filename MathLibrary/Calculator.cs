aa27648ddc742bd9497d651f83ab06294997fa73
tatiana-nazz <tatiananazz7@gmail.com>
2025-06-11 15:37:10 +0300

    Initial commit - base project

---
+++ MathLibrary/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace MathLibrary;
+
+/// <summary>
+/// يحتوي على دوال رياضية بسيطة
+/// </summary>
+public static class Calculator
+{
+    /// <summary>
+    /// تحسب حاصل ضرب عددين صحيحين
+    /// </summary>
+    /// <param name="a">العدد الأول</param>
+    /// <param name="b">العدد الثاني</param>
+    /// <returns>ناتج الضرب</returns>
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
